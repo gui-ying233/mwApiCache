0902ab5eb0fcb58f7f99a18f71ef69f3ee8df93e
gui-ying233 <zhuyufei20050307@gmail.com>
2024-11-11 12:56:11 -0500

    支持POST当GET用的情况

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         萌娘百科缓存部分Api请求
 // @namespace    https://github.com/gui-ying233/mwApiCache
-// @version      1.0.0
+// @version      1.1.0
 // @description  缓存部分Api请求结果7日以提升速度减少WAF几率
 // @author       鬼影233
 // @license      MIT
@@ -21,11 +21,12 @@
 		if (!window.mediaWiki?.Api?.prototype) return;
 		clearInterval(id);
 		const originalMediaWikiApiGet = window.mediaWiki.Api.prototype.get;
+		const originalMediaWikiApiPost = window.mediaWiki.Api.prototype.post;
 		await window.$.ready;
 		const cfg = window.mediaWiki.config;
 		const userName = cfg.get("wgUserName");
 		const timestamp = Date.now();
-		const getCache = (t, arg) => {
+		const getCache = (t, method, arg) => {
 			const _arg = JSON.stringify(arg);
 			const cache = JSON.parse(
 				localStorage.getItem(`mwApiCache-${_arg}`)
@@ -35,7 +36,7 @@
 				cache.ver !== ver ||
 				timestamp - cache.timestamp > 1000 * 60 * 60 * 24 * 7
 			) {
-				const res = originalMediaWikiApiGet.call(t, arg);
+				const res = method.call(t, arg);
 				res.then(_res => {
 					console.debug(
 						`%cmwApiCache-Set\n${_arg}\n${JSON.stringify(_res)}`,
@@ -61,13 +62,14 @@
 				.promise()
 				.then(() => cache.res);
 		};
-		window.mediaWiki.Api.prototype.get = function (...args) {
+		const apiFilter = (t, method, args) => {
 			const arg = JSON.stringify(args[0]);
 			switch (arg) {
 				case `{"action":"query","ususers":"${userName}","meta":["userinfo","siteinfo"],"list":["users"],"uiprop":["rights"],"siprop":["specialpagealiases"],"usprop":["blockinfo"]}`:
 				case '{"action":"query","meta":"siteinfo","siprop":"specialpagealiases","formatversion":2,"uselang":"content","maxage":3600}':
 				case '{"action":"query","meta":"userinfo","uiprop":["groups","rights"]}':
 				case `{"action":"query","prop":"revisions","titles":"User:${userName}/codemirror-mediawiki.json","rvprop":"content","rvlimit":1}`:
+				case `{"action":"query","assertuser":"${userName}","list":"allusers","augroup":"special-contributor|goodeditor|manually-confirmed|extendedconfirmed|ipblock-exempt|flood|bot|file-maintainer|techeditor|honoredmaintainer|patroller|interface-admin|sysop|suppress|checkuser|bureaucrat|staff","aulimit":"max","auprop":"groups","aufrom":"Outloudvi"}`:
 				case `{"action":"query","meta":"allmessages","ammessages":["Editnotice-${cfg.get(
 					"wgNamespaceNumber"
 				)}","Editnotice-${cfg.get("wgNamespaceNumber")}-${cfg
@@ -81,20 +83,26 @@
 						}:`,
 						""
 					)}"],"amenableparser":1}`:
-					return getCache(this, args[0]);
+					return getCache(t, method, args[0]);
 				default:
 					if (
 						/{"action":"query","meta":"allmessages","ammessages":\[".*?"\],"amlang":"zh","formatversion":2}/.test(
 							arg
 						)
 					)
-						return getCache(this, args[0]);
+						return getCache(t, method, args[0]);
 					console.debug(
 						`%cmwApiCache-Ign\n${arg}`,
 						"border-left:1em solid #4E3DA4;background-color:#3C2D73;color:#D9D9D9;padding:1em"
 					);
-					return originalMediaWikiApiGet.apply(this, args);
+					return method.apply(t, args);
 			}
+		};
+		window.mediaWiki.Api.prototype.get = function (...args) {
+			return apiFilter(this, originalMediaWikiApiGet, args);
+		};
+		window.mediaWiki.Api.prototype.post = function (...args) {
+			return apiFilter(this, originalMediaWikiApiPost, args);
 		};
 	}, 5);
 })();
