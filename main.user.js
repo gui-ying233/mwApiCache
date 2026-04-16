f27393b8db3d91f2e3c031f825f4958240880b66
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2026-04-16 02:51:30 -0400

    GET -> POST

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         萌娘百科缓存部分Api请求
 // @namespace    https://github.com/gui-ying233/mwApiCache
-// @version      3.10.0
+// @version      3.10.1
 // @description  缓存部分Api请求结果以提升速度减少WAF几率
 // @author       鬼影233
 // @license      MIT
@@ -247,7 +247,7 @@
 			return result;
 		};
 		window.mediaWiki.Api.prototype.get = function (...args) {
-			return apiFilter(this, originalMediaWikiApiGet, args);
+			return apiFilter(this, originalMediaWikiApiPost, args);
 		};
 		window.mediaWiki.Api.prototype.post = function (...args) {
 			return apiFilter(this, originalMediaWikiApiPost, args);
